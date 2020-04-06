0504dfc5b7a5e7f8e7696d5bbd54456b57b953e8
mario <mario496luisin@gmail.com>
2020-04-06 16:20:17 +0200

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,28 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    'https://api.darksky.net/forecast/91c8918ab512860b6e7d4d8c1fc79610/' +
+    latitude +
+    ',' +
+    longitude;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          ' It is currently ' +
+          body.currently.temperature +
+          ' degrees out. There is a ' +
+          body.currently.precipProbability +
+          '% chance of rain.'
+      );
+    }
+  });
+};
+
+module.exports = forecast;
